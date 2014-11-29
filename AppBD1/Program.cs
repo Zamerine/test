200a923f94d33a1af1917d362e9f7838b635fad6
Zamerine <ptitlelex@hotmail.com>
2014-11-29 10:55:35 +0100

    hjbkjl

---
+++ AppBD1/Program.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBD1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new masterEntities())
+            {
+                foreach(var t in context.Tables)
+                    Console.WriteLine(t.LastName+ "  " + t.Id);
+                    
+            }
+
+            DAL.DataAccess.OpenConnection();
+
+            string[] columnTest = { "Id", "Libelle", "Country" };
+
+            string[] param = {"213","Kikoulol","France"};
+            DAL.DataAccess.InsertValues("Test", columnTest, param);
+            
+            Console.WriteLine(DAL.DataAccess.SelectContent("Test", columnTest));
+
+            DAL.DataAccess.CloseConnexion();
+            AttendreAvantFermetureConsole();
+        }
+
+        private static void AttendreAvantFermetureConsole()
+        {
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ DAL/DataAccess.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    
+    public class DataAccess
+    {
+        private static SqlConnection connection;
+        public static void OpenConnection()
+        {
+            connection = new SqlConnection(@"Data Source=(LocalDb)\v11.0;Integrated Security=True");
+            connection.Open();
+        }
+        public static void CloseConnexion()
+        {
+            connection.Close();
+        }
+        public static void InsertValues(string table, string [] columnName, string [] param)
+        {
+            string insertString = "INSERT INTO " + table + "(";
+
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                insertString += String.Format("{0}", columnName[i]);
+                if (i == (columnName.Length - 1))
+                    insertString += ") VALUES (";
+                else
+                    insertString += ", ";
+            }
+
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                insertString += String.Format("@{0}", columnName[i]);
+                if (i == (columnName.Length - 1))
+                    insertString += ");";
+                else
+                    insertString += ", ";
+            }
+
+            SqlCommand insert = new SqlCommand(insertString, connection);
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                insert.Parameters.AddWithValue(String.Format("@{0}", columnName[i]), param[i]);
+            }
+            insert.ExecuteNonQuery();
+        }
+        public static string SelectContent(string table, string [] columnName)
+        {
+            string returnString = "";
+            
+            string selectString = "SELECT ";
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                selectString += String.Format("{0}", columnName[i]); 
+                if (i == (columnName.Length - 1))
+                    selectString += " ";
+                else
+                    selectString += ", ";
+            }       
+
+            selectString += "FROM " + table;
+            SqlCommand select = new SqlCommand(selectString, connection);
+            SqlDataReader reader = select.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < columnName.Length; i++)
+                {
+                    returnString += String.Format("{0} ", reader[i]);
+                }
+                returnString += "\n";
+            }
+            reader.Close();
+            return returnString;
+        }
+    }
+}
